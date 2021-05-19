2cc55dc06b1277ab9b5ed9c1b5baf374ef9e100b
BShaq <34344089+BShaq@users.noreply.github.com>
2021-05-19 16:05:47 +0200

    Update sc-draft.sol

--- smart-contract/sc-draft.sol
+++ smart-contract/sc-draft.sol
@@ -33,7 +33,7 @@ contract Roulette is VRFConsumerBase {
     /**
      * Constructor inherits VRFConsumerBase
      * 
-     * Network: Rinkeby
+     * Network: Kovan
      * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
